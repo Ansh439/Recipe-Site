cc419d17769dbf65060e5251a54cbc56fa95ba83
Ansh439 <gupta.ansh3150@gmail.com>
2024-07-28 02:20:50 +0530

    added functionalities in Signup Page

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGO_URI)
         console.log(error);
     })
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(express.json());
 
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,14 +1,55 @@
-import { Button, Label, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Signup() {
+  const [formData, setFormData] = useState({});
+  const [errMessage, setErrMessage] = useState(null);
+  const [loading, setloading] = useState(false);
+
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id] : e.target.value.trim()});
+  }
+
+
+  const handelSubmit = async(e) => {
+    e.preventDefault();
+
+    if(!formData.username || !formData.email || !formData.password){
+      setErrMessage("Please fill out all the fields");
+      return ;
+    }
+
+    try{
+      setloading(true);
+      setErrMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {'Content-Type' : 'application/json'},
+        body: JSON.stringify(formData),
+      })
+      const data = await res.json();
+      console.log(data);
+      if(data.success === false){
+        return setErrMessage(data.message);
+      }
+      setloading(false);
+      if(res.ok){
+        navigate('/signin');
+      }
+    }catch(err){
+      setErrMessage(data.message);
+      setloading(false);
+    }
+  }
+
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
         <div className='flex-1'>
           <Link to='/' className='font-bold dark:text-white text-4xl'>
-          <span className='px-2 py-1 bg-gradient-to-r from-green-400 via-lime-400 to-blue-300 rounded-lg text-white md:mr-1'>
+            <span className='px-2 py-1 bg-gradient-to-r from-green-400 via-lime-400 to-blue-300 rounded-lg text-white md:mr-1'>
                 Quick
             </span>
             Cook
@@ -18,39 +59,58 @@ export default function Signup() {
           </p>
         </div>
         <div className='flex-1'>
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4' onSubmit={handelSubmit}>
             <div>
               <Label value='Enter Username' />
               <TextInput
               type='text'
               placeholder="username"
               id="username"
+              onChange={handleChange}
               />
             </div>
             <div>
               <Label value='Enter Email' />
               <TextInput
-              type='text'
+              type='email'
               placeholder="email"
               id="email"
+              onChange={handleChange}
               />
             </div>
             <div>
               <Label value='Enter Password' />
               <TextInput
-              type='text'
+              type='password'
               placeholder="password"
               id="password"
+              onChange={handleChange}
               />
             </div>
-            <Button gradientDuoTone='greenToBlue' type='submit'>Sign Up</Button>
+            <Button gradientDuoTone='greenToBlue' type='submit' disabled={loading}>
+              {
+                loading ? (
+                  <>
+                    <Spinner size='sm' />
+                    <span className='pl-3'>Loading...</span>
+                  </>                    
+                ) : 'Sign Up'
+              }
+            </Button>
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Have an acoount?</span>
-            <Link to='/signin' className='text-green-500 hover:underline font-semibold'>
+            <Link to='/signin' className='text-green-500 font-semibold hover:underline'>
               Sign In
             </Link>
           </div>
+          {
+            errMessage && (
+              <Alert className='mt-5' color='failure'>
+                {errMessage}
+              </Alert>
+            )
+          }
         </div>
       </div>
     </div>
