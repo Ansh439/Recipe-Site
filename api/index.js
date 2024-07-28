818853676b994789f35ddd0343f55bce28573db8
Ansh439 <gupta.ansh3150@gmail.com>
2024-07-29 00:15:21 +0530

    port change

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGO_URI)
         console.log(error);
     })
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
