11a97d4edd5afe1e7423f8e50a41bea372d672cb
Ansh439 <gupta.ansh3150@gmail.com>
2024-07-28 18:18:40 +0530

    Showing post on user dashboard

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { verifyUserToken } from '../utils/verifyUserToken.js';
-import { create } from '../controllers/post.controller.js';
+import { create, getposts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyUserToken, create);
+router.get('/getposts', getposts)
 
 export default router;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,7 +3,6 @@ import { useLocation } from 'react-router-dom'
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
 import { SiCodechef } from "react-icons/si";
-import DashPost from '../components/DashPosts';
 import DashPosts from '../components/DashPosts';
 
 export default function Dashboard() {
